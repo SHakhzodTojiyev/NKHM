f88116b7de0a3ee97a0eb7066868f31898f52d7a
milliarderodam <66087615+milliarderodam@users.noreply.github.com>
2022-11-04 11:28:23 +0500

    Update posts.js

--- data/posts.js
+++ data/posts.js
@@ -14,5 +14,12 @@ export const posts = [
     date: "Octabr 3, 2022",
     post: "Bu yangi 2 post edi, Bu yangi 2 post edi..., Bu yangi 2 post edi...,Bu yangi 2 post edi...,Bu yangi 2 post edi...,Bu yangi 2 post edi...kkkkkkkkkkkkkkkkkk"
 },
+ {
+    id: "/single-post-3.html",
+    img: "img/content/content.png",
+    title: "Bu yangi post 3",
+    date: "Noyabr 3, 2022",
+    post: "Bu yangi 3 post edi, Bu yangi 3 post edi..., Bu yangi 2 post edi...,Bu yangi 2 post edi...,Bu yangi 2 post edi...,Bu yangi 2 post edi...kkkkkkkkkkkkkkkkkk"
+},
 ];
 
